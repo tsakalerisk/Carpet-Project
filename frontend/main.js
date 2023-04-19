3e3f987ed4990432cd4c102f8613b8d97449e39c
Konstantinos Tsakaleris <13333629+tsakalerisk@users.noreply.github.com>
2023-04-19 18:16:05 +0300

    Light mode support and clean up javascript

--- frontend/main.js
+++ frontend/main.js
@@ -1,55 +1,58 @@
+const CONTACTS_PER_PAGE = 20
+
+let data, page = 0
+
 let getData = async () => {
     let response = await fetch('./MOCK_DATA.json')
-    let data = await response.json()
-    let table = document.querySelector('#contacts')
-
-    let updateTable = data => {
-        table.innerHTML = data.map((x, index) => `
-            <tr>
-                <td>${x.first_name}</td>
-                <td>${x.last_name}</td>
-                <td>${x.street_address}</td>
-                <td>${x.city}</td>
-                <td>${x.state}</td>
-                <td>${x.zip_code}</td>
-                <td>${x.phone_number}</td>
-                <td>${x.email}</td>
-                <td class="contact-options">
-                    <button class="edit" onclick="showUpdateForm('${JSON.stringify(x).replaceAll('"', '%')}')">
-                        <span class="material-symbols-outlined">edit_square</span>
-                    </button>
-                    <button class="delete" onclick="console.log('Deleted ${index}. Replace this with id + delete request.')">
-                        <span class="material-symbols-outlined">delete</span>
-                    </button>
-                </td>
-            </tr>
-        `).join('')
-    }
-
-    updateTable(data.slice(0, 20))
-
-    let filterText = document.querySelector('.filter')
-    filterText.oninput = e => {
-        updateTable(
-            data
-                .filter(x => x.first_name.toLowerCase().includes(e.target.value.toLowerCase()))
-                .slice(0, 20)
-        )
-    }
+    data = await response.json()
+    updateTable(data.sort((a, b) => a.first_name > b.first_name ? 1 : -1).slice(0, 20))
 }
 
 getData()
 
-let showUpdateForm = input => {
-    let contact = JSON.parse(input.replaceAll('%', '"'))
-    let form = document.querySelector('.update-form')
-    form.classList.add('open')
+let updateTable = data => {
+    let table = document.querySelector('#contacts')
+    table.innerHTML = data.map(x => `
+        <tr>
+            <td>${x.first_name}</td>
+            <td>${x.last_name}</td>
+            <td>${x.street_address}</td>
+            <td>${x.city}</td>
+            <td>${x.state}</td>
+            <td>${x.zip_code}</td>
+            <td>${x.phone_number}</td>
+            <td>${x.email}</td>
+            <td class="contact-options">
+                <button class="edit" onclick="showUpdateForm(${x.id})">
+                    <span class="material-symbols-outlined">edit_square</span>
+                </button>
+                <button class="delete" onclick="console.log('Deleted id=${x.id}. Replace this with delete request.')">
+                    <span class="material-symbols-outlined">delete</span>
+                </button>
+            </td>
+        </tr>
+    `).join('')
+}
 
+let filterText = document.querySelector('.filter')
+filterText.oninput = e => {
+    updateTable(
+        data
+            .filter(x => x.first_name.toLowerCase().includes(e.target.value.toLowerCase()))
+            .slice(0, 20)
+    )
+}
+
+let showUpdateForm = id => {
+    let form = document.querySelector('.update-form')
+    let contact = data.find(x => x.id === id)
     //fill out all fields in the form with the contact's data
     Object.keys(contact).forEach(key => {
         form.querySelector(`#${key}`) &&
             (form.querySelector(`#${key}`).value = contact[key])
     })
+
+    form.classList.add('open')
 }
 
 let hideUpdateForm = () => document.querySelector('.update-form').classList.remove('open')
