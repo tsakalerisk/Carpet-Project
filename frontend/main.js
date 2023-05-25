53a2e1e976e67b45307715b16bc70548a5c671f5
Konstantinos Tsakaleris <13333629+tsakalerisk@users.noreply.github.com>
2023-05-25 23:44:03 +0300

    Replace div modal with dialog, Improve CSS

--- frontend/main.js
+++ frontend/main.js
@@ -1,29 +1,32 @@
 const CONTACTS_PER_PAGE = 20
 const FieldNames = {
-    first_name: "First name",
-    last_name: "Last name",
-    street_address: "Street address",
-    city: "City",
-    state: "State",
-    zip_code: "ZIP code",
-    phone_number: "Phone number",
-    email: "Email",
+    first_name: 'First name',
+    last_name: 'Last name',
+    street_address: 'Street address',
+    city: 'City',
+    state: 'State',
+    zip_code: 'ZIP code',
+    phone_number: 'Phone number',
+    email: 'Email',
 }
 
-let data, current_page = 1
+let data,
+    current_page = 1
 let filters = Object.fromEntries(new URLSearchParams(document.location.search))
 
 let loadFilters = () => {
     let container = document.querySelector('.filters')
     container.innerHTML = Object.keys(filters)
-        .map(key => `
+        .map(
+            key => `
             <div class="filter">
                 ${FieldNames[key]}: ${filters[key]}
                 <span class="material-symbols-outlined" onclick="removeFilter('${key}')">
                     close
                 </span>
             </div>
-        `)
+        `
+        )
         .join('')
 }
 
@@ -31,12 +34,11 @@ loadFilters()
 
 let getData = async () => {
     let response = await fetch('./MOCK_DATA.json')
-    data = (await response.json())
-        .filter(
-            contact => Object.keys(filters).every(
-                key => contact[key].toLowerCase().startsWith(filters[key].toLowerCase())
-            )
+    data = (await response.json()).filter(contact =>
+        Object.keys(filters).every(key =>
+            contact[key].toLowerCase().startsWith(filters[key].toLowerCase())
         )
+    )
     updateTable(data)
     updatePages(data)
 }
@@ -46,9 +48,13 @@ getData()
 let updateTable = data => {
     let table = document.querySelector('#contacts')
     table.innerHTML = data
-        .sort((a, b) => a.first_name > b.first_name ? 1 : -1)
-        .slice((current_page - 1) * CONTACTS_PER_PAGE, current_page * CONTACTS_PER_PAGE)
-        .map(x => `
+        .sort((a, b) => (a.first_name > b.first_name ? 1 : -1))
+        .slice(
+            (current_page - 1) * CONTACTS_PER_PAGE,
+            current_page * CONTACTS_PER_PAGE
+        )
+        .map(
+            x => `
         <tr>
             <td>${x.first_name}</td>
             <td>${x.last_name}</td>
@@ -67,21 +73,33 @@ let updateTable = data => {
                 </button>
             </td>
         </tr>
-    `)
+    `
+        )
         .join('')
 }
 
 let updatePages = data => {
     let paging = document.querySelector('.paging')
     let mapToButton = x => `
-    <button class="${x === current_page ? 'current' : ''}" onclick="setPage(${x})" ${x === current_page ? 'disabled' : ''}>
+    <button class="${
+        x === current_page ? 'current' : ''
+    }" onclick="setPage(${x})" ${x === current_page ? 'disabled' : ''}>
     ${x}
     </button>
     `
-    let pages = Array.from({ length: Math.ceil(data.length / CONTACTS_PER_PAGE) }, (_, i) => i + 1)
+    let pages = Array.from(
+        { length: Math.ceil(data.length / CONTACTS_PER_PAGE) },
+        (_, i) => i + 1
+    )
     paging.innerHTML = pages
-        .filter(x => x === 1 || x === pages.length || Math.abs(current_page - x) < 2)
-        .flatMap((x, i, filtered_pages) => filtered_pages[i + 1] > x + 1 ? [mapToButton(x), '. . .'] : [mapToButton(x)])
+        .filter(
+            x => x === 1 || x === pages.length || Math.abs(current_page - x) < 3
+        )
+        .flatMap((x, i, filtered_pages) =>
+            filtered_pages[i + 1] > x + 1
+                ? [mapToButton(x), '. . .']
+                : [mapToButton(x)]
+        )
         .join('')
 }
 
@@ -89,47 +107,51 @@ let setPage = page => {
     current_page = page
     updateTable(data)
     updatePages(data)
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0)
 }
 
+let formModal = document.querySelector('.contact-form-container')
+
 let showNewForm = () => {
-    let form = document.querySelector('.contact-form-container')
-    form.querySelector('.contact-form').reset()
+    formModal.querySelector('.contact-form').reset()
     document.querySelector('.contact-header').innerHTML = 'New Contact'
-    form.classList.add('open')
+    formModal.showModal()
 }
 
 let showUpdateForm = id => {
-    let form = document.querySelector('.contact-form-container')
     let contact = data.find(x => x.id === id)
     //fill out all fields in the form with the contact's data
     Object.keys(contact).forEach(key => {
-        form.querySelector(`#${key}`) &&
-            (form.querySelector(`#${key}`).value = contact[key])
+        if (formModal.querySelector(`#${key}`))
+            formModal.querySelector(`#${key}`).value = contact[key]
     })
     document.querySelector('.contact-header').innerHTML = 'Edit Contact'
-    form.classList.add('open')
+    formModal.showModal()
 }
 
-let hideUpdateForm = () => document.querySelector('.contact-form-container').classList.remove('open')
+let hideUpdateForm = () => formModal.close()
 
-window.addEventListener('click', e => {
-    let form = document.querySelector('.contact-form-container')
-    if (!form.contains(e.target) && !e.target.closest('.opens-form') && form.classList.contains('open')) {
-        hideUpdateForm()
-    }
-})
+formModal.onclick = e => {
+    const dialogDim = formModal.getBoundingClientRect()
+    if (
+        e.clientX < dialogDim.left ||
+        e.clientX > dialogDim.right ||
+        e.clientY < dialogDim.top ||
+        e.clientY > dialogDim.bottom
+    )
+        formModal.close()
+}
 
 let filterForm = document.querySelector('.filter-form')
 filterForm.onsubmit = e => {
     e.preventDefault()
     let newFilters = new URLSearchParams(document.location.search)
     newFilters.set(e.target.filter_key.value, e.target.filter_value.value)
-    window.location = "./?" + newFilters.toString()
+    window.location = './?' + newFilters.toString()
 }
 
 let removeFilter = filter => {
     let newFilters = new URLSearchParams(document.location.search)
     newFilters.delete(filter)
-    window.location = "./?" + newFilters.toString()
+    window.location = './' + (newFilters.size ? '?' + newFilters.toString() : '')
 }
